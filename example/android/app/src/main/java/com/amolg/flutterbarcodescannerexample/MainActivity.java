8f8053b440a1acde180497c75401db264e0b40e2
Samuel Nde <samund@microsoft.com>
2023-02-04 18:22:20 -0800

    Changed Flutter Activity to Activity.

--- example/android/app/src/main/java/com/amolg/flutterbarcodescannerexample/MainActivity.java
+++ example/android/app/src/main/java/com/amolg/flutterbarcodescannerexample/MainActivity.java
@@ -1,10 +1,10 @@
 package com.amolg.flutterbarcodescannerexample;
 
-//import io.flutter.embedding.android.FlutterActivity;
-import io.flutter.embedding.android.FlutterFragmentActivity;
+import io.flutter.embedding.android.FlutterActivity;
+//import io.flutter.embedding.android.FlutterFragmentActivity;
 
 
-public class MainActivity extends FlutterFragmentActivity {
+public class MainActivity extends FlutterActivity {
   // You can keep this empty class or remove it. Plugins on the new embedding
   // now automatically registers plugins.
 }
